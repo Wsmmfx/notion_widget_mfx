0395aba5526ff15a46091b4a07cf9d2ac6b71354
Wsmmfx <150179939+Wsmmfx@users.noreply.github.com>
2024-01-24 16:24:33 +0100

    Create tr.js

---
+++ tr.js
@@ -0,0 +1,36 @@
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+  {
+  "symbols": [
+    {
+      "description": "",
+      "proName": "OANDA:EURUSD"
+    },
+    {
+      "description": "",
+      "proName": "OANDA:GBPUSD"
+    },
+    {
+      "description": "",
+      "proName": "OANDA:AUDUSD"
+    },
+    {
+      "description": "",
+      "proName": "OANDA:NZDUSD"
+    },
+    {
+      "description": "",
+      "proName": "CAPITALCOM:US100"
+    }
+  ],
+  "isTransparent": false,
+  "showSymbolLogo": true,
+  "colorTheme": "light",
+  "locale": "en"
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
